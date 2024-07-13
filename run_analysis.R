95e3aeec597d9ea0e4187c5659f17c0186257cba
pog <pooriagolestanipg@gmail.com>
2024-07-13 18:46:29 +0330

    there were som problem with filtering out unwanted columns which has been fixed and the tidy dataset of averages of wanted columns has been added

--- run_analysis.R
+++ run_analysis.R
@@ -66,10 +66,73 @@ rm("train_obs_sub_file")
 train_data <- train_data %>% mutate(subject = train_obs_sub, .before = activity)
 
 ### removing columns other than columns containing means and stds
-#train_data <- train_data %>% select(c(contains(c("std", "mean")) , ))
+train_data <- train_data %>% select( c( subject, activity, contains(c(".mean..", ".std..")) ) )
 
 
+############
 
 
+## loading test data
+
+### loading test features
+test_data <- read.table("data/UCI HAR Dataset/test/X_test.txt", col.names = features)
+test_data <- tbl_df(test_data)
+
+### adding train activity class to train data
+test_activity_file <- file("data/UCI HAR Dataset/test/y_test.txt", "r")
+test_activities <- as.numeric(readLines(test_activity_file))
+close(test_activity_file, "r")
+rm("test_activity_file")
+
+test_activities <- sapply(test_activities, function(activity){
+      activities[activity]
+})
+
+test_data <- test_data %>% mutate(activity = test_activities, .before = tBodyAcc.mean...X)
+
+### adding subjects of observations to train_data
+test_obs_sub_file <- file("data/UCI HAR Dataset/test/subject_test.txt", "r")
+test_obs_sub <- as.character(readLines(test_obs_sub_file))
+close(test_obs_sub_file, "r")
+rm("test_obs_sub_file")
+
+test_data <- test_data %>% mutate(subject = test_obs_sub, .before = activity)
+
+### removing columns other than columns containing means and stds
+test_data <- test_data %>% select( c( subject, activity, contains(c(".mean..", ".std..")) ) )
+
+############################################################################
+
+# combinint test and train data
+data_total <- rbind(train_data, test_data)
+
+#grouping dataset by activity and subjects
+grouped_data <- data_total %>% group_by(subject, activity)
+
+# averaging(summarizing) grouped data for each group(for different activities of each subject)
+summarized_data <- grouped_data %>% summarize_at(vars(tBodyAcc.mean...X:fBodyBodyGyroJerkMag.std..), mean, na.rm=T)
+
+# cleaning final data variable names
+names(summarized_data) <- gsub("\\.\\.", "\\(\\)", names(summarized_data))
+names(summarized_data) <- gsub("\\.", "\\_", names(summarized_data))
+#names(summarized_data) <- gsub("\\.$", "", names(summarized_data))
+
+# replacing names of final data with more discriptive names
+names(summarized_data) <- c(names(summarized_data)[1:2], paste0("mean_of_" , names(summarized_data)[3:length(names(summarized_data))]))
+
+# separating average of means and stds into to table and saving them
+summarized_of_mean <- summarized_data %>% select( c( subject, activity, contains("_mean") ) )
+summarized_of_std <- summarized_data %>% select( c( subject, activity, contains("_std")))
+
+# writing tidy datsets to a file
+if(!dir.exists("./data/tidy_data/")) dir.create("./data/tidy_data/")
+
+file.create("./data/tidy_data/mean_of_means")
+write.table(summarized_of_mean, file("./data/tidy_data/mean_of_means" , "w"))
+close("./data/tidy_data/mean_of_means" , "w")
+
+file.create("./data/tidy_data/mean_of_stds")
+write.table(summarized_of_std, file("./data/tidy_data/mean_of_stds" , "w"))
+close("./data/tidy_data/mean_of_stds" , "w")
 
 
